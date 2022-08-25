5197d652dce8d9aa60086bc4eb2331fec4d22192
Tonny Blair Okumu <hellotony2034@gmail.com>
2022-08-26 01:12:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    return customerName = (customerName.toLocaleUpperCase())
+}
+function setBestCustomer() {
+    return bestCustomer = 'not bob'
+}
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob'
+} 
+const leastFavoriteCustomer = "valueA"
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "new variable"
+}
